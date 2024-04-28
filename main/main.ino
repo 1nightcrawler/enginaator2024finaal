e219d14d5d038d90bc4fd76700e896f4b51e7540
theUGGO <joonatanjj@gmail.com>
2024-04-28 23:33:55 +0300

    eemaldasin ebavajaliku rea

--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,17 @@
-
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
 #include "menu.h"
-
-TFT_eSPI tft = TFT_eSPI();
 
 #include "game1.h"
 
 extern TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-<<<<<<< HEAD
     tft.init();
     tft.setRotation(0);
     tft.fillScreen(TFT_RED);
-=======
     menu(tft);
->>>>>>> d0fffc30110d419ebd7e3627713b9e3e254a1e4a
 }
 
 void loop() {
